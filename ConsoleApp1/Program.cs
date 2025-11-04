65abb4d9ed8d9f93d09cd18a051c0d3a53efe99d
mcaleg2006 <emil720apro@gmail.com>
2025-11-04 14:51:32 +0200

    Binary-Search-Algorithm

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,9 +1,37 @@
 ﻿
+
+public class Solution {
+    public int SearchInsert(int[] nums, int target)
+    {
+        var min = 0;
+        var max = nums.Length - 1;
+        while (min <= max)
+        {
+            var mid = (min + max) / 2;
+            var found = nums[mid];
+            if (found == target)
+            {
+                return mid;
+            }
+            else if (found > target)
+            {
+                max = mid - 1; // Search in the right half
+            }
+            else
+            {
+                min = mid + 1; // Search in the left half
+            }
+        }
+        return min;
+    }
+}
+
+
 class Program
 {
     static void Main(string[] args)
     {
-        // Solution s = new Solution();
-        // Console.WriteLine(s.IsPalindrome(123123));
+        Solution s = new Solution();
+        Console.WriteLine(s.SearchInsert([1,3,5,6], 2));
     }
 }
