75142bdfea3021b027971e76398631c53aca3ae8
mcaleg2006 <emil720apro@gmail.com>
2025-11-09 15:50:45 +0200

    MergeTwoLinkedList

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -71,6 +71,39 @@ public class Solution {
             return list2;
         }
     }
+
+    public double GetVal(ListNode list1)
+    {
+        if (list1 == null)
+        {
+            return double.PositiveInfinity;
+        }
+
+        return (double)list1.val;
+    }
+    
+
+    public ListNode MergeTwoLists3(ListNode list1, ListNode list2)
+    {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        while (list1 != null || list2 != null)
+        {
+            if (GetVal(list1) < GetVal(list2))
+            {
+                curr.next = list1;
+                list1 = list1.next;
+            }
+            else
+            {
+                curr.next = list2;
+                list2 = list2.next;
+            }
+            curr = curr.next;
+        }
+        return dummy.next;
+    }
         
         
     
