4f3aa31120eaf10710bf18741e39325ce95ac641
mcaleg2006 <emil720apro@gmail.com>
2025-11-09 11:23:52 +0200

    LinkedPalindrome

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -14,18 +14,55 @@ public class ListNode
 
 public class Solution
 {
-    // f 
-    // 1 -> 2 -> 3 -> 4 -> 5 -> null
-    // s
+    public bool IsPalindrome(ListNode head)
+    {
+         ListNode first_half_end_node = middleNode(head);
+         ListNode second_half_begin_node = reverseList(first_half_end_node);
+         
+         //     p1          p2
+         // in: 1 ->  2  -> 3
+        // out: 1 ->  2  <- 3 
+        
+        
+        //     p1             p2
+        // in: 1 -> 2 -> 3 -> 4
+        // out:1 -> 2 <- 3 <- 4
+        //              /
+        //        null<|
+        
+        
+         ListNode p1 = head;
+         ListNode p2 = second_half_begin_node;
+
+         while (p1 != null && p2 != null)
+         {
+             if (p1.Data != p2.Data)
+             {
+                 return false;
+             }
+
+             p1 = p1.Next;
+             p2 = p2.Next;
+         }
+         return true;
+    }
+
+    public ListNode reverseList(ListNode head)
+    {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null)
+        {
+            ListNode temp = curr;
+            curr = curr.Next;
+            temp.Next = prev;
+            prev = temp;
+        }
+        return prev;
+    }
     
-    //           f
-    // 1 -> 2 -> 3 -> 4 -> 5 -> null
-    // s
     
-    
-    //                     f
-    // 1 -> 2 -> 3 -> 4 -> 5 -> null
-    //           s
     public ListNode middleNode(ListNode head)
     {
         ListNode slow = head;
@@ -39,6 +76,7 @@ public class Solution
         return slow;
     }
 }
+
 class Program
 {
     static void Main(string[] args)
